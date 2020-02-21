e2c91c6eb3c06cf5b6ae8fb19bf7bf23b51685c4
Jose <jose@runroom.com>
2020-02-21 18:02:39 +0100

    Adds animation to component

--- src/components/jsComponent/index.js
+++ src/components/jsComponent/index.js
@@ -1,17 +1,24 @@
 import React from "react";
 
+import { Button } from './styles';
+
 class JSComponent extends React.Component {
+  state = {
+    playing: true
+  };
+
   constructor(props) {
     super(props);
 
     const AFRAME = window.AFRAME;
     const THREE = window.THREE;
 
-    AFRAME.registerComponent('plane', {
+    AFRAME.registerComponent('custom-box', {
       schema: {
-        width: {type: 'number', default: 1},
-        height: {type: 'number', default: 1},
-        color: {type: 'color', default: '#AAA'}
+        width: { type: 'number', default: 1 },
+        height: { type: 'number', default: 1 },
+        depth: { type: 'number', default: 1 },
+        color: { type: 'color', default: '#ffff00' }
       },
       init: function () {
         console.log('Init');
@@ -19,26 +26,53 @@ class JSComponent extends React.Component {
         var data = this.data;
         var el = this.el;
 
-        const geometry = new THREE.PlaneGeometry(data.width, data.height);
-        const material = new THREE.MeshBasicMaterial({ color: 0xffff00, side: THREE.DoubleSide });
+        const geometry = new THREE.BoxBufferGeometry(data.width, data.height, data.depth);
+        const material = new THREE.MeshBasicMaterial({ color: data.color });
         const mesh = new THREE.Mesh(geometry, material);
 
         el.setObject3D('mesh', mesh);
+      },
+      tick: function () {
+        const increment = THREE.Math.radToDeg(this.el.object3D.rotation.x) + 1;
+
+        this.el.object3D.rotation.x = THREE.Math.degToRad(increment % 359);
+      },
+      play() {
+        console.log('Playing...');
+      },
+      pause() {
+        console.log('Pause!');
       }
     });
   }
 
+  updateTick = () => {
+    if (this.state.playing === true) {
+      this.entity.pause();
+      this.setState({ playing: false });
+    } else {
+      this.entity.play();
+      this.setState({ playing: true });
+    }
+  }
+
   render() {
     return (
-      <a-scene>
-        <a-marker preset="hiro">
-          <a-entity
-            plane="width: 1; height: 1; color: orange"
-            position="0 0 0"
-            rotation="-90 0 0"
-          ></a-entity>
-        </a-marker>
-      </a-scene>
+      <>
+        <Button onClick={this.updateTick}>
+          {this.state.playing ? 'Pause' : 'Play'}
+        </Button>
+        <a-scene>
+          <a-marker preset="hiro">
+            <a-entity
+              ref={ref => this.entity = ref}
+              custom-box="width: 1; height: 1; depth: 1; color: orange;"
+              position="0 0 0"
+              rotation="-90 0 0"
+            ></a-entity>
+          </a-marker>
+        </a-scene>
+      </>
     );
   }
 }
---
+++ src/components/jsComponent/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  position: absolute;
+  top: 30px;
+  left: 30px;
+  zIndex: 10;
+`;
+
+export { Button };

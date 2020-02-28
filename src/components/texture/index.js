8d36bc7f1b9c20908b571854d0fff744038bdc20
Jose <jose@runroom.com>
2020-02-28 19:07:25 +0100

    Adds basic fragment and vertex shader

--- src/components/texture/index.js
+++ src/components/texture/index.js
@@ -1,5 +1,24 @@
 import React from "react";
 
+const vertexShader = `
+  uniform float time;
+  uniform vec2 resolution;
+
+  void main()	{
+      gl_Position = vec4( position, 1.0 );
+  }
+`;
+const fragmentShader = `
+  uniform float time;
+  uniform vec2 resolution;
+
+  void main()	{
+      float x = mod(time + gl_FragCoord.x, 20.) < 10. ? 1. : 0.;
+      float y = mod(time + gl_FragCoord.y, 20.) < 10. ? 1. : 0.;
+      gl_FragColor = vec4(vec3(min(x, y)), 1.);
+  }
+`;
+
 class JSComponent extends React.Component {
   state = {
     playing: true
@@ -22,7 +41,15 @@ class JSComponent extends React.Component {
         var el = this.el;
 
         const geometry = new THREE.PlaneGeometry(data.width, data.height);
-        const material = new THREE.MeshBasicMaterial({ color: data.color, side: THREE.DoubleSide });
+        // const material = new THREE.MeshBasicMaterial({ color: data.color, side: THREE.DoubleSide });
+        const material = new THREE.ShaderMaterial({
+          uniforms: {
+            time: { type: "f", value: 1.0 },
+            resolution: { type: "v2", value: new THREE.Vector2() }
+          },
+          vertexShader,
+          fragmentShader
+        });
         const mesh = new THREE.Mesh(geometry, material);
 
         el.setObject3D('mesh', mesh);
@@ -35,7 +62,7 @@ class JSComponent extends React.Component {
       <a-scene>
         <a-marker preset="hiro">
           <a-entity
-            custom-texture="width: 3; height: 1.5; color: orange;"
+            custom-texture="width: 1; height: 1; color: orange;"
             position="0 0 0"
             rotation="-90 0 0"
           ></a-entity>
---
+++ src/components/texture/vertex.js
@@ -0,0 +1,10 @@
+const vertex = `
+  uniform float time;
+  uniform vec2 resolution;
+
+  void main()	{
+      gl_Position = vec4( position, 1.0 );
+  }
+`;
+
+export default vertex;

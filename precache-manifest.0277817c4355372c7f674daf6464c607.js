d1415ee4d0acd4e3db42fe4401787046dad22f3a
Jose <jose@runroom.com>
2020-03-20 16:35:57 +0100

    Updates

---
+++ precache-manifest.0277817c4355372c7f674daf6464c607.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd94d7871363b6dcfd7174108c564e3",
+    "url": "/augmented-reality/index.html"
+  },
+  {
+    "revision": "7326af7df16d25cd0547",
+    "url": "/augmented-reality/static/js/2.e452f2cf.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/augmented-reality/static/js/2.e452f2cf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8c09008e2b5e4a386cf0",
+    "url": "/augmented-reality/static/js/main.765196fe.chunk.js"
+  },
+  {
+    "revision": "e2a24235f348f72b0a7f",
+    "url": "/augmented-reality/static/js/runtime-main.9541b78e.js"
+  }
+]);

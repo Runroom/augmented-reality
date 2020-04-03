3e7cb7d1f26dbd59aa741b31b3f4e7c8b8b3ea8e
Jose <jose@runroom.com>
2020-04-03 16:28:44 +0200

    Shows text with weather details

--- src/arComponents/Weather/index.js
+++ src/arComponents/Weather/index.js
@@ -1,61 +1,45 @@
 import React from 'react';
 
 import withScene from '../../hoc/withScene';
+
+import { capitalize } from '../../utils/helpers';
 import getWeather from '../../utils/weather';
+import theme from '../../config/theme';
 
 const WRAP_COUNT = 25;
 const WIDTH = 3;
 
 class Weather extends React.Component {
   state = {
-    text: false
+    text: 'Example text'
   }
 
   componentDidMount() {
     getWeather('Barcelona')
       .then(weather => {
-        this.setState({ weather });
         const today = new Date(weather.date);
 
-        let text = `${weather.description}`;
+        let text = `${capitalize(weather.description)}\n`;
         text += `City: ${weather.city}, ${weather.country}\n`;
         text += `Today: ${today.getDate()}-${(today.getMonth() + 1)}-${today.getFullYear()}\n`;
         text += `Temperature: ${weather.max} - ${weather.min}\n`;
 
-        console.log(text);
-
-        this.setState({ weather: text });
+        this.setState({ text });
       });
   }
 
   render() {
     const { text } = this.state;
 
-    // return weather && (
-    //   <>
-    //     <p>{`City: ${weather.city}, ${weather.country}`}</p>
-    //     <p>{`Today: ${today.getDate()}-${(today.getMonth() + 1)}-${today.getFullYear()}`}</p>
-    //     <p>{`Temperature: ${weather.max} - ${weather.min}`}</p>
-    //     <p>{`Description: ${weather.description}`}</p>
-    //   </>
-    // );
-
-    return text && (
+    return (
       <a-marker-camera preset="hiro">
         <a-entity
-          position={`-${WIDTH / 2} 0 0`}
-        >
-          <a-text
-            geometry={`primitive: plane; width: ${WIDTH}; height: auto;`}
-            color="red"
-            material="color: transparent; opacity: 0;"
-            value={text}
-            position="0 0 0"
-            wrap-count={WRAP_COUNT}
-            align="left"
-            anchor="left"
-          ></a-text>
-        </a-entity>
+          geometry={`primitive: plane; height: ${WIDTH / 3}; width: auto`}
+          material={`color: ${theme.colors.neutro100}`}
+          text={`width: ${WIDTH}; wrap-count: ${WRAP_COUNT}; value: ${text}; color: ${theme.colors.neutro900}; align: left;`}
+          position="0 0 0"
+          scale=".5 .5 .5"
+        ></a-entity>
       </a-marker-camera>
     );
   }

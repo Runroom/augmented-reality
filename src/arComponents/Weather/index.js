2fc2d3cc76fa7abc2503eea43fb6441b1f761ad4
Jose <jose@runroom.com>
2020-04-03 14:36:41 +0200

    Adds text component showing weather info

--- src/arComponents/Weather/index.js
+++ src/arComponents/Weather/index.js
@@ -1,17 +1,64 @@
-import React from "react";
+import React from 'react';
 
-class Video extends React.Component {
-  // componentDidMount() {
+import withScene from '../../hoc/withScene';
+import getWeather from '../../utils/weather';
 
-  // }
+const WRAP_COUNT = 25;
+const WIDTH = 3;
+
+class Weather extends React.Component {
+  state = {
+    text: false
+  }
+
+  componentDidMount() {
+    getWeather('Barcelona')
+      .then(weather => {
+        this.setState({ weather });
+        const today = new Date(weather.date);
+
+        let text = `${weather.description}`;
+        text += `City: ${weather.city}, ${weather.country}\n`;
+        text += `Today: ${today.getDate()}-${(today.getMonth() + 1)}-${today.getFullYear()}\n`;
+        text += `Temperature: ${weather.max} - ${weather.min}\n`;
+
+        console.log(text);
+
+        this.setState({ weather: text });
+      });
+  }
 
   render() {
-    return (
-      <>
-        {"Weather"}
-      </>
+    const { text } = this.state;
+
+    // return weather && (
+    //   <>
+    //     <p>{`City: ${weather.city}, ${weather.country}`}</p>
+    //     <p>{`Today: ${today.getDate()}-${(today.getMonth() + 1)}-${today.getFullYear()}`}</p>
+    //     <p>{`Temperature: ${weather.max} - ${weather.min}`}</p>
+    //     <p>{`Description: ${weather.description}`}</p>
+    //   </>
+    // );
+
+    return text && (
+      <a-marker-camera preset="hiro">
+        <a-entity
+          position={`-${WIDTH / 2} 0 0`}
+        >
+          <a-text
+            geometry={`primitive: plane; width: ${WIDTH}; height: auto;`}
+            color="red"
+            material="color: transparent; opacity: 0;"
+            value={text}
+            position="0 0 0"
+            wrap-count={WRAP_COUNT}
+            align="left"
+            anchor="left"
+          ></a-text>
+        </a-entity>
+      </a-marker-camera>
     );
   }
 }
 
-export default Video;
+export default withScene(Weather);
